0d90f05a29c652cc5046073d5d10ad32daae69cb
Antti Haavikko <antti.haavikko@gmail.com>
2023-11-11 12:45:43 +0200

    Change skill icon display width on uno mode

--- Assets/Scripts/SkillDefinition.cs
+++ Assets/Scripts/SkillDefinition.cs
@@ -8,39 +8,20 @@ using UnityEngine.Serialization;
 [CreateAssetMenu(fileName = "Skill", menuName = "Skill", order = 0)]
 public class SkillDefinition : ScriptableObject
 {
-    [ScriptableObjectId] [SerializeField] private string id;
     [SerializeField] private Skill data;
 
     public string Title => data.title;
 
     public Skill Spawn()
     {
-        var d = data;
-        d.id = id;
-        return d;
+        return data;
     }
 
     public bool CanGet(IEnumerable<Skill> existing)
     {
-        return existing.Count(s => s.id == id) <= data.allowedRepeats;
+        return existing.Count(s => s.title == Title) <= data.allowedRepeats;
     }
 }
-
-public class ScriptableObjectIdAttribute : PropertyAttribute { }
-
-#if UNITY_EDITOR
-[CustomPropertyDrawer(typeof(ScriptableObjectIdAttribute))]
-public class ScriptableObjectIdDrawer : PropertyDrawer {
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-        GUI.enabled = false;
-        if (string.IsNullOrEmpty(property.stringValue)) {
-            property.stringValue = Guid.NewGuid().ToString();
-        }
-        EditorGUI.PropertyField(position, property, label, true);
-        GUI.enabled = true;
-    }
-}
-#endif
 
 public enum Effect
 {
@@ -54,7 +35,6 @@ public enum Effect
 [Serializable]
 public struct Skill
 {
-    [HideInInspector] public string id;
     public Effect effect;
     public string title;
     [TextArea] public string description;
