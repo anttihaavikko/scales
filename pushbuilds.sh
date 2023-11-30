c8378377a11facb14a6eb977727ecb5ed8c4fbb1
Antti Haavikko <antti.haavikko@gmail.com>
2023-11-30 20:14:47 +0200

    Update build pushing script

--- pushbuilds.sh
+++ pushbuilds.sh
@@ -4,15 +4,15 @@ NC='\033[0m' # No Color
 echo " "
 
 echo "${COLOR}Pushing build for Windows${NC}"
-butler push Builds/win anttihaavikko/replace:win --fix-permissions
+butler push Builds/win anttihaavikko/arithmometer:win --fix-permissions
 
 echo "${COLOR}Pushing build for OSX${NC}"
-butler push Builds/osx anttihaavikko/replace:osx --fix-permissions
+butler push Builds/osx anttihaavikko/arithmometer:osx --fix-permissions
 
 echo "${COLOR}Pushing build for Linux${NC}"
-butler push Builds/linux anttihaavikko/replace:linux
+butler push Builds/linux anttihaavikko/arithmometer:linux
 
-echo "${COLOR}Copying html5 files over to correct path"
-cp -a Builds/webgl/html5/Build/. Builds/html5/Build
+#echo "${COLOR}Copying html5 files over to correct path"
+#cp -a Builds/webgl/html5/Build/. Builds/html5/Build
 echo "${COLOR}Pushing build for HTML5${NC}"
-butler push Builds/html5 anttihaavikko/replace:html5
+butler push Builds/html5 anttihaavikko/arithmometer:html5
