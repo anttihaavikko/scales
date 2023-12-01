1d8ab1a681b609578f6ea78bfc61189e141b89fe
Antti Haavikko <antti.haavikko@gmail.com>
2023-12-01 15:38:31 +0200

    Fix web build path

--- pushbuilds.sh
+++ pushbuilds.sh
@@ -13,6 +13,6 @@ echo "${COLOR}Pushing build for Linux${NC}"
 butler push Builds/linux anttihaavikko/arithmometer:linux
 
 echo "${COLOR}Copying html5 files over to correct path"
-cp -a Builds/webgl/html5/Build/. Builds/html5/Build
+cp -a Builds/webgl/Arithmometer/Build/. Builds/html5/Build
 echo "${COLOR}Pushing build for HTML5${NC}"
 butler push Builds/html5 anttihaavikko/arithmometer:html5
